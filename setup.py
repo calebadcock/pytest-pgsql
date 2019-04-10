0765fdd45ae2c735fa47240c3589fb431f5667e2
Caleb Adcock <caleb.m.adcock@gmail.com>
2019-04-10 16:47:22 -0400

    Bad fix

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import setup
 import sys
+import os
 
 # Check the Python version manually because pip < 9.0 doesn't check it for us.
 if sys.version_info < (3, 4):
     raise RuntimeError('Unsupported version of Python: ' + sys.version)
 
+old_pbr_version = os.getenv('PBR_VERSION') or ''
+os.environ['PBR_VERSION'] = '1.1.1'
+
 setup(
     setup_requires=['pbr'],
     pbr=True,
 )
+
+os.environ['PBR_VERSION'] = old_pbr_version
